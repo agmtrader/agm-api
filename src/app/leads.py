74e11fa38dacc7cd1bea812349be2ee2fd251ca3
Andres Aguilar <aguilarcarboni@gmail.com>
2025-04-11 14:08:23 -0600

    Update leads definition

--- src/app/leads.py
+++ src/app/leads.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request
-from src.components.leads import read_leads, update_lead, create_lead
+from src.components.leads import read_leads, update_lead, create_lead, delete_lead
 from src.utils.scope_manager import verify_scope, enforce_user_filter
 
 bp = Blueprint('leads', __name__)
@@ -29,3 +29,11 @@ def update_route():
     data = payload['data']
     query = payload.get('query', None)
     return update_lead(data=data, query=query)
+
+@bp.route('/delete', methods=['POST'])
+@verify_scope('leads/delete')
+@enforce_user_filter()
+def delete_route():
+    payload = request.get_json(force=True)
+    query = payload.get('query', None)
+    return delete_lead(query=query)
--- src/components/leads.py
+++ src/components/leads.py
@@ -11,11 +11,14 @@ class Lead:
     def __init__(self, data):
         self.lead_id = data['LeadID']
         self.contact_date = data['ContactDate']
+        print(data)
         self.name = data['Name']
+        self.referrer = data['Referrer']
+        self.phone_country = data['PhoneCountry']
         self.phone = data['Phone']
         self.email = data['Email']
         self.description = data['Description']
-        self.followup_date = data['FollowupDate']
+        self.followups = data['FollowUps']
         self.completed = data['Completed']
 
     def to_dict(self):
@@ -23,10 +26,12 @@ class Lead:
             'LeadID': self.lead_id,
             'ContactDate': self.contact_date,
             'Name': self.name,
+            'Referrer': self.referrer,
+            'PhoneCountry': self.phone_country,
             'Phone': self.phone,
             'Email': self.email,
             'Description': self.description,
-            'FollowupDate': self.followup_date,
+            'FollowUps': self.followups,
             'Completed': self.completed
         }
         
@@ -44,4 +49,9 @@ def read_leads(query=None):
 @handle_exception
 def update_lead(data, query=None):
     lead = Database.update(path='db/leads/uploaded', data=data, query=query)
+    return lead
+
+@handle_exception
+def delete_lead(query=None):
+    lead = Database.delete(path='db/leads/uploaded', query=query)
     return lead
