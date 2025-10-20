35ab1a2ef7732006ebc14a680564a3e48f3ea1e3
Andres <aa@agmtechnology.com>
2025-10-20 13:25:05 -0600

    Update Supabase model to allow autoincrement for code field
    - Modified the 'code' column in the Supabase model to be nullable and set it to autoincrement, enhancing data integrity and management.
    - Added missing import for identity from sqlalchemy.orm to support future enhancements.

--- src/utils/connectors/supabase.py
+++ src/utils/connectors/supabase.py
@@ -3,6 +3,8 @@ from sqlalchemy.dialects.postgresql import UUID, JSONB, ARRAY
 import uuid
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
+
+from sqlalchemy.orm import identity
 from src.utils.managers.database_manager import DatabaseManager
 from src.utils.logger import logger
 from src.utils.managers.secret_manager import get_secret
@@ -69,7 +71,7 @@ class Supabase:
             contact_id = Column(UUID(as_uuid=True), ForeignKey('contact.id', ondelete='SET NULL', onupdate='CASCADE'), nullable=True)
             created = Column(Text, nullable=False, default=datetime.now().strftime('%Y%m%d%H%M%S'))
             updated = Column(Text, nullable=False, default=datetime.now().strftime('%Y%m%d%H%M%S'))
-            code = Column(Integer, nullable=False, unique=True)
+            code = Column(Integer, nullable=True, unique=True, autoincrement=True)
             agency = Column(Text, nullable=False)
             hierarchy1 = Column(Text, nullable=False)
             hierarchy2 = Column(Text, nullable=False)
