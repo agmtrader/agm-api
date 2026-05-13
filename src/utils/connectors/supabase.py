888519a59db01deb69b637f7a53851e392f4c52a
Andres Aguilar <aguilarcarboni@gmail.com>
2026-05-13 10:09:44 -0600

    feat: Add created and updated timestamps to NewAccountContact and NewContact models; adjust account_id nullability in NewContact

--- src/utils/connectors/supabase.py
+++ src/utils/connectors/supabase.py
@@ -137,6 +137,8 @@ class Supabase:
         class NewAccountContact(self.Base):
             __tablename__ = 'new_account_contact'
             id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+            created = Column(Text, nullable=False, default=datetime.now().strftime('%Y%m%d%H%M%S'))
+            updated = Column(Text, nullable=False, default=datetime.now().strftime('%Y%m%d%H%M%S'))
             contact_id = Column(UUID(as_uuid=True), ForeignKey('new_contact.id', ondelete='SET NULL', onupdate='CASCADE'), nullable=False)
             account_id = Column(UUID(as_uuid=True), ForeignKey('new_account.id', ondelete='SET NULL', onupdate='CASCADE'), nullable=False)
             entity_id = Column(BIGINT, nullable=True)
@@ -158,7 +160,7 @@ class Supabase:
             id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
             created = Column(Text, nullable=False, default=datetime.now().strftime('%Y%m%d%H%M%S'))
             updated = Column(Text, nullable=False, default=datetime.now().strftime('%Y%m%d%H%M%S'))
-            account_id = Column(UUID(as_uuid=True), ForeignKey('new_account.id', ondelete='SET NULL', onupdate='CASCADE'), nullable=False)
+            account_id = Column(UUID(as_uuid=True), ForeignKey('new_account.id', ondelete='SET NULL', onupdate='CASCADE'), nullable=True)
             contact_id = Column(UUID(as_uuid=True), ForeignKey('new_contact.id', ondelete='SET NULL', onupdate='CASCADE'), nullable=True)
             document_id = Column(UUID(as_uuid=True), ForeignKey('document.id', ondelete='SET NULL', onupdate='CASCADE'), nullable=False)
             category = Column(Text, nullable=True)
